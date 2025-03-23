f1a4f249a8e436586fd04274d41e1f1681ee7fca
Luka- <luka-groot@hotmail.nl>
2025-03-23 18:24:39 +0100

    start on dispersion curve class

--- measurement_obj.py
+++ measurement_obj.py
@@ -123,29 +123,34 @@ class Measurement:
 
 class DispersionCurve:
 
-    def __init__(self):
-        pass
+    def __init__(self, modes):
+        self.modes = modes
+
+    
+    @staticmethod
+    def from_dlr_txts(path):
+        modes = []
+        for path in data_dir.glob('*.txt'):
+            curves = pd.read_csv(path)
+            
+            # In the exported csvs, "Attenuation" is always the last column of a mode
+            modes_in_class = sum([1 for col_name in curves.columns if "Attenuation" in col_name])
+            columns_per_mode = len(curves.columns) // modes_in_class
+            mode_freq_headers = curves.columns[0::columns_per_mode]
+            mode_phase_vel_headers = curves.columns[1::columns_per_mode]
+            # TODO make this work for anisotropic 
+            for i, mode_freq in enumerate(mode_freq_headers):
+                freq_vel = curves[[mode_freq, mode_phase_vel_headers[i]]].dropna()
+                freq_vel = freq_vel.sort_values(mode_freq)
+
+                spline = interpolate.CubicSpline(freq_vel[mode_freq], freq_vel[mode_phase_vel_headers[i]], extrapolate=False)
+                modes.append(spline)
+        
 
 if __name__ == '__main__':
     data_dir = pathlib.Path(__file__).parent / 'data' / 'dispersion_curves' / 's355j2_dispersion_curves'
     print(data_dir)
-    modes = []
-    for path in data_dir.glob('*.txt'):
-        curves = pd.read_csv(path)
-        
-        # In the exported csvs, "Attenuation" is always the last column of a mode
-        modes_in_class = sum([1 for col_name in curves.columns if "Attenuation" in col_name])
-        columns_per_mode = len(curves.columns) // modes_in_class
-        mode_freq_headers = curves.columns[0::columns_per_mode]
-        mode_phase_vel_headers = curves.columns[1::columns_per_mode]
-        # TODO make this work for anisotropic 
-        for i, mode_freq in enumerate(mode_freq_headers):
-            freq_vel = curves[[mode_freq, mode_phase_vel_headers[i]]].dropna()
-            freq_vel = freq_vel.sort_values(mode_freq)
-
-            spline = interpolate.CubicSpline(freq_vel[mode_freq], freq_vel[mode_phase_vel_headers[i]], extrapolate=False)
-            modes.append(spline)
-        
+    
     # data_dir_ab = pathlib.Path(__file__).parent / 'data' / 'measurement_data'/ 'abaqus_test_steel'
     # sig = load_signals_abaqus(data_dir_ab)[0]
 
@@ -164,7 +169,7 @@ if __name__ == '__main__':
     avg_signals = load_signals_SINTEG(data_sinteg, skip_idx={}, plot_outliers=True)
 
     avg_signals = [sig.zero_average_signal().bandpass(30e3, 90e3) for sig in avg_signals]
-    measurement = Measurement((0,0), [(18e-3,0), (58e-3, 0.), (182e-3, 0.)], avg_signals[-1], avg_signals[:-1], modes)
+    measurement = Measurement((0,0), [(18e-3,0), (58e-3, 0.), (182e-3, 0.)], avg_signals[-1], avg_signals[:-1])
     measurement.compare_signals(1, 2)
 
 
